956b43610b65477c7793ce0f83c5f795b6923a3d
Alaric <alaric.hadef@weezevent.com>
2019-11-07 22:09:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,233 @@
+import React from 'react';
+import './App.css';
+import {
+  languages,
+  fetchRepositories
+} from '@huchenme/github-trending';
+import { Button, Image, Segment, Header, Dropdown, Divider, Dimmer, Loader, Container, Card, Icon} from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css'
+
+
+function App() {
+  const [repos, setRepos] = React.useState([]);
+  const [lang, setLang] = React.useState(null);
+  const [myRepos, setMyRepos] = React.useState([]);
+  const [langsDrop, setLangsDrop] = React.useState(null);
+  const [period, setPeriod] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  const periodOptions = [
+    {
+      key : 'daily',
+      text: 'daily',
+      value: 'daily'
+    },
+    {
+      key : 'weekly',
+      text: 'weekly',
+      value: 'weekly'
+    },
+    {
+      key : 'monthly',
+      text: 'monthly',
+      value: 'monthly'
+    }
+  ];
+
+  React.useEffect(() => {
+
+    const mapToLanguages = ({ name }) => ({ key: name, text: name, value: name });
+    let tmpLanguages = languages.map(mapToLanguages);
+    setLangsDrop(tmpLanguages);
+    let tmpRepos = localStorage.getItem('myRepos');
+    if(tmpRepos){
+      setMyRepos(JSON.parse(tmpRepos));
+    };
+  }, []);
+  
+  React.useEffect(() => {
+    setLoading(true);
+    fetchRepositories({language: lang, period: period}).then(repositories => {
+      console.log(repositories);
+      setRepos([...repositories]);
+    }).catch(e => {
+      console.warn(e);
+    }).finally(() => {
+      setLoading(false);
+    });
+  }, [lang, period]);
+
+  const addToFavourite = React.useCallback((repo) => {
+    console.warn('repo', repo);
+    let tmpRepos = [...myRepos];
+    console.warn(tmpRepos);
+    tmpRepos.push(repo);
+    setMyRepos(tmpRepos);
+    localStorage.setItem('myRepos', JSON.stringify(tmpRepos));
+    tmpRepos = [...repos].filter(myrepo => myrepo.name !== repo.name);
+    setRepos(tmpRepos);
+
+  }, [myRepos, setMyRepos, repos]);
+
+  const removeFromFavourite = React.useCallback((repo) => {
+    console.warn('tmprepos before', repos);
+    let tmpRepos=[...repos].filter(tmprepo => tmprepo.name !== repo.name);
+    tmpRepos.push(repo);
+    console.warn('tmprepos', tmpRepos);
+    setRepos(tmpRepos);
+    let tmpMyRepos = [...myRepos].filter(myrepo => myrepo.name !== repo.name);
+    setMyRepos(tmpMyRepos);
+    localStorage.setItem('myRepos', JSON.stringify(tmpMyRepos));
+  },[myRepos, repos]);
+
+  const renderRepos = React.useCallback(() => {
+    let tab = [];
+    let tmpRepos = myRepos.map(repo => repo.name);
+    repos.filter(repo =>
+      !tmpRepos.includes(repo.name)
+    ).forEach(repo => {
+      tab.push(
+      <Card link
+        color="green"
+      >
+        <Card.Content
+          onClick={()=> window.open(repo.url, "_blank")}
+        >
+          <Card.Header>
+            {repo.name}
+          </Card.Header>
+          <Card.Meta>
+            {repo.author}
+          </Card.Meta>
+          <Card.Description>
+          <Image
+            floated='right'
+            size='mini'
+            src={repo.avatar}
+          />
+            {repo.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>
+            {repo.stars} <Icon name='star' color='yellow' disabled />
+            {repo.forks} <Icon name='fork' color='black' disabled />
+            <br/>{repo.language ? `Developped in ${repo.language}` : "No language related"}
+          </p>
+      </Card.Content>
+        <Card.Content extra>
+          <Button basic color='green'
+            onClick={() => addToFavourite(repo)}
+          >
+            Add to favourite
+          </Button>
+      </Card.Content>
+
+      </Card>
+      )
+    });
+    return tab;
+  },[myRepos, repos, addToFavourite]);
+
+  const renderMyRepos = React.useCallback(() => {
+    let tab = [];
+    myRepos.forEach(repo => {
+      tab.push(
+      <Card link
+        color="red"
+      >
+        <Card.Content
+          onClick={()=> window.open(repo.url, "_blank")}
+        >
+          <Card.Header>
+            {repo.name}
+          </Card.Header>
+          <Card.Meta>
+            {repo.author}
+          </Card.Meta>
+          <Card.Description>
+          <Image
+            floated='right'
+            size='mini'
+            src={repo.avatar}
+          />
+            {repo.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>
+            {repo.stars} <Icon name='star' color='yellow' disabled />
+            {repo.forks} <Icon name='fork' color='black' disabled />
+            <br/>{repo.language ? `Developped in ${repo.language}` : "No language related"}
+          </p>
+      </Card.Content>
+
+        <Card.Content extra>
+          <Button basic color='red'
+            onClick={() => removeFromFavourite(repo)}
+          >
+            Remove from favourite
+          </Button>
+      </Card.Content>
+      </Card>
+      )
+    });
+    return tab;
+  }, [myRepos, removeFromFavourite]);
+
+  return (
+    <>
+    <Container>
+      <Header>
+
+      </Header>
+      <Segment>
+        <Dropdown
+            clearable
+            fluid
+            search
+            options={langsDrop}
+            selection
+            value={lang}
+            onChange={(synth, data) => setLang(data.value)}
+            placeholder="Pick a language"
+        />
+        <Divider/>
+        <Dropdown
+            clearable
+            fluid
+            search
+            options={periodOptions}
+            selection
+            value={period}
+            onChange={(synth, data) => setPeriod(data.value)}
+            placeholder="Pick a period"
+        />
+      </Segment>
+    {loading && <Dimmer active inverted>
+        <Loader>
+            Loading ...
+        </Loader>
+    </Dimmer>}
+    <Header as='h2' icon textAlign='center'>
+      <Icon name='favorite' circular />
+      {myRepos.length ? <Header.Content>My saved repositories</Header.Content> :  <Header.Content>No repositories saved, start browsing !</Header.Content>}
+    </Header>
+     <Card.Group doubling itemsPerRow={5} stackable>
+      {renderMyRepos()}
+      </Card.Group>
+      <Divider/>
+      <Header as='h2' icon textAlign='center'>
+      <Icon name='github' circular />
+      {repos.length ? <Header.Content>Explore repositories</Header.Content> :  <Header.Content>No repos found, start a new search !</Header.Content> }
+    </Header>
+    {!loading && <Card.Group doubling itemsPerRow={5} stackable>
+      {renderRepos()}
+      </Card.Group>}
+    </Container>
+    </>
+  );
+
+}
+
+export default App;
